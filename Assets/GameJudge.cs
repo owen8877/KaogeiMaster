8a606820b73e364f165b167df3dc76958c1fa006
owen8877 <owentoget@yeah.net>
2017-05-07 03:15:03 +0800

    Game Over!

--- Assets/GameJudge.cs
+++ Assets/GameJudge.cs
@@ -9,15 +9,30 @@ public class GameJudge : MonoBehaviour {
     public GameObject TopCard, MidCard, BottomCard;
     public GameObject aiJiang;
     public RawImage RankingImage;
+    public GameObject gameOver;
     float life, score;
     bool onGoing = false;
     const float MAX_LIFE = 100, MAX_SCORE = 100;
+
+    AiControllerGame aiControllerGame;
+    AiControllerGame.EyeData eyeData;
+    AiControllerGame.EyeBrowData eyeBrowData;
+    AiControllerGame.LipData lipData;
+    AiControllerGame.OtherData otherData;
+    AiControllerGame.NeckData neckData;
 
     void Start() {
         lifeMask = lifeGameObject.transform.Find("lifeMask").gameObject;
         scoreMask = scoreGameObject.transform.Find("scoreMask").gameObject;
 
         reset();
+
+        aiControllerGame = aiJiang.GetComponent<AiControllerGame>();
+        eyeData = aiControllerGame.eyeData;
+        eyeBrowData = aiControllerGame.eyeBrowData;
+        lipData = aiControllerGame.lipData;
+        otherData = aiControllerGame.otherData;
+        neckData = aiControllerGame.neckData;
     }
 
     const int BAR_HEIGHT = 450;
@@ -61,8 +76,10 @@ public class GameJudge : MonoBehaviour {
             return;
 
         if (Time.time > nextJudgeTime) {
-            judge();
+            Ranking ranking = judge();
             round++;
+            setRanking(ranking);
+            updateRanking(true);
             if (round >= MAX - 1) {
                 onGoing = false;
             } else {
@@ -74,34 +91,54 @@ public class GameJudge : MonoBehaviour {
         showScore(score);
         updateTargetImagePosition(round);
         takeDownScore();
+        updateRanking(false);
+
+        if (life < 0) {
+            onGoing = false;
+            updateGameOver(true);
+        }
     }
 
     void takeDownScore() {
         // magicScore comes from eyeData, eyeBrowData, lipData, otherData
-        AiControllerGame aiControllerGame = aiJiang.GetComponent<AiControllerGame>();
-        AiControllerGame.EyeData eyeData = aiControllerGame.eyeData;
-        AiControllerGame.EyeBrowData eyeBrowData = aiControllerGame.eyeBrowData;
-        AiControllerGame.LipData lipData = aiControllerGame.lipData;
-        AiControllerGame.OtherData otherData = aiControllerGame.otherData;
-        AiControllerGame.NeckData neckData = aiControllerGame.neckData;
-        float magicScore = 0;
+        float magicScore = 100 * Random.value;
         scoreList.Add(magicScore);
     }
 
     float calculateScore() {
         // use scoreList and round
+        float sum = 0;
+        foreach (float f in scoreList)
+            sum += f;
+
+        float result = sum / scoreList.Count;
         scoreList.Clear();
-        return 85;
+        return result;
     }
 
-    void judge() {
+    Ranking judge() {
         //Debug.Log("ha!");
-        float thisScore = calculateScore();
+        float thisScore = 10 * Mathf.Sqrt(calculateScore());
+        Debug.Log(thisScore);
+ 
         if (thisScore > GOOD_SCORE) {
             score += 20;
+            life += 10;
         } else {
-            life -= 20;
+            life -= 10;
         }
+
+        if (thisScore > 90)
+            return Ranking.SS;
+        if (thisScore > 80)
+            return Ranking.S;
+        if (thisScore > 70)
+            return Ranking.A;
+        if (thisScore > 60)
+            return Ranking.B;
+        if (thisScore > 50)
+            return Ranking.C;
+        return Ranking.D;
     }
 
     public void realStart() {
@@ -115,9 +152,57 @@ public class GameJudge : MonoBehaviour {
         round = 0;
         score = 0;
         onGoing = false;
+        updateGameOver(false);
     }
 
-    void showRanking(int ranking) {
-        
+    enum Ranking {
+        SS, S, A, B, C, D
+    };
+
+    void setRanking(Ranking ranking) {
+        Vector2 xy = Vector2.zero, offset = Vector2.zero;
+        switch (ranking) {
+            case Ranking.A:
+                xy = new Vector2(0f, 0.5f);
+                offset = new Vector2(0.33f, 0.5f);
+                break;
+            case Ranking.B:
+                xy = new Vector2(0.34f, 0.5f);
+                offset = new Vector2(0.33f, 0.5f);
+                break;
+            case Ranking.C:
+                xy = new Vector2(0.66f, 0.5f);
+                offset = new Vector2(0.33f, 0.5f);
+                break;
+            case Ranking.D:
+                xy = new Vector2(0f, 0f);
+                offset = new Vector2(0.33f, 0.5f);
+                break;
+            case Ranking.S:
+                xy = new Vector2(0.32f, 0f);
+                offset = new Vector2(0.33f, 0.5f);
+                break;
+            case Ranking.SS:
+                xy = new Vector2(0.66f, 0f);
+                offset = new Vector2(0.33f, 0.5f);
+                break;
+        }
+        RankingImage.uvRect = new Rect(xy[0], xy[1], offset[0], offset[1]);
+    }
+
+    void updateRanking(bool force) {
+        float gamma = 0.03f;
+        if (force)
+            RankingImage.color = new Vector4(RankingImage.color.r, RankingImage.color.g, RankingImage.color.b, 1);
+        else
+            RankingImage.color = new Vector4(RankingImage.color.r, RankingImage.color.g, RankingImage.color.b, RankingImage.color.a * (1 - gamma));
+        //Debug.Log(RankingImage.color.a);
+    }
+
+    void updateGameOver(bool over) {
+        if (over)
+            gameOver.GetComponent<Text>().color = new Vector4(199/256f, 15/256f, 15/256f, 255/256f);
+        else
+            gameOver.GetComponent<Text>().color = new Vector4(199/256f, 15/256f, 15/256f, 0f);
     }
 }
